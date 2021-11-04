f96413d3fe359033b16981920bad37a0cfb72f94
Dylan Floyd <floyd800@gmail.com>
2021-11-04 16:09:42 -0700

    red

---
+++ index.test.js
@@ -0,0 +1,21 @@
+describe('http getMethod tests', () => {
+
+  test('calling getMethod with a GET request returns GET', () => {
+    const actual = getMethod(
+      'GET / HTTP/1.1\n\r' +
+      'Host: example.com\n\r' +
+      'Accept-Language: us-en');
+    const expected = 'GET';
+    
+    expect(actual).toEqual(expected);
+  })
+
+  test('calling getMethod with a DELETE request returns DELETE', () => {
+    const actual = getMethod(
+      'DELETE / HTTP/1.1' +
+      'Host: example.com' +
+      'Accept-Language: us-en');
+    const expected = 'DELETE';
+    expect(actual).toEqual(expected);
+  });
+});
